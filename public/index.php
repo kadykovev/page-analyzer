263158351688d6745c1048c0b46e75f8d3245c66
Евгений Кадыков <kadykov_e_v@mail.ru>
2024-01-15 21:34:32 +0300

    Add index.phtml

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello World!";
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+//$app->add(MethodOverrideMiddleware::class);
+$app->addErrorMiddleware(true, true, true);
+
+
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->run();
