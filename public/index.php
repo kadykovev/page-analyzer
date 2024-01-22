bf6192b452d48011a92bf2ff182d04431ca4668a
Евгений Кадыков <kadykov_e_v@mail.ru>
2024-01-22 14:11:39 +0300

    Fixing the database connection render.com

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@ $container->set('pdo', function () {
     $username = $databaseUrl['user'];
     $password = $databaseUrl['pass'];
     $host = $databaseUrl['host'];
-    $port = $databaseUrl['port'];
+    $port = $databaseUrl['port'] ?? null;
     $dbName = ltrim($databaseUrl['path'], '/');
 
     if (isset($port)) {
